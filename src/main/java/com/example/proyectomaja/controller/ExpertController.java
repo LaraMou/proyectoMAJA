e85c4c15151094fbdb443ab68370e0459f3bf7ad
Monica Lara <ptts.no2020@gmail.com>
2021-04-30 22:20:59 +0200

    vercel y deploy

--- src/main/java/com/example/proyectomaja/controller/ExpertController.java
+++ src/main/java/com/example/proyectomaja/controller/ExpertController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = { "http://localhost:4200" })
+@CrossOrigin(origins = { "https://proyecto-final-monica-lara.vercel.app" },methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 @RestController
 @RequestMapping("/api")
 public class ExpertController {
